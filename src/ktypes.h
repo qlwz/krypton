560fb46ac07e38881134b27e18a003de3274c328
Alexander Alashkin <alexander.alashkin@cesanta.com>
2016-04-28 16:10:38 +0200

    Use Krypton in MIOT/Posix by default
    PUBLISHED_FROM=d714ecd86923c3fe1a7f183142474ac2c8db93c9

--- src/ktypes.h
+++ src/ktypes.h
@@ -7,7 +7,9 @@
 #define CS_KRYPTON_SRC_KTYPES_H_
 
 #define _FILE_OFFSET_BITS 64
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 #undef WIN32_LEAN_AND_MEAN  // Let windows.h always include winsock2.h
 
 #ifndef NS_INTERNAL
